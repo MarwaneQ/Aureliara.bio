8f4c56b84bf3afbb9bb213f847edd754aa610a58
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-06-26 18:47:12 +0100

    restart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
 import Preload from "./components/preload";
+import Hero from "./components/hero";
+import Product from "./components/product";
+import About from "./components/about";
 export default function Home() {
   return (
     <main>
         <Preload />
+        <Product />
+         <About /> 
     </main>
   );
 }
