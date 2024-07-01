e61277af9933548c6488054762198c8ee3f5c710
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-07-01 11:28:29 +0100

    social media icon

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -5,10 +5,10 @@ import aboutImage from "../assets/about-img.jpg";
 import logo2 from "../assets/landing2.png";
 export default function About() {
   return (
-    <div id="about" className="mt-32">
+    <div id="about" className="mt-40 mb-32">
       <div className="w-full  px-2  flex items-center justify-evenly bg-white md:flex-row lg:flex-row flex-col">
         <div className="flex flex-col items-center  justify-center p-5  md:mt-0 text-center md:text-start ">
-      <h3>About</h3>
+          <h3>About</h3>
           <Image
             src={logo2}
             layout=""
@@ -20,8 +20,8 @@ export default function About() {
           </h4>
           <div className="flex justify-start flex-col">
             <p className="md:w-[45rem] mb-2">
-              Welcome to Aureliara, where we believe that true beauty radiates
-              from within and is reflected in the care you give to your skin.
+              we believe that true beauty radiates from within and is reflected
+              in the care you give to your skin.
               <br />
               Our mission is to illuminate your natural glow through premium,
               science-backed skincare solutions that harmonize with nature’s
