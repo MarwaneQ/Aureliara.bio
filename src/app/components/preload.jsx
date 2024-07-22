ec2284b40e4ce9fae2105650e881b1a94815cbe1
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-07-22 22:10:04 +0100

    shop link

--- src/app/components/preload.jsx
+++ src/app/components/preload.jsx
@@ -97,7 +97,9 @@ export default function Preload() {
                     type="button"
                     ref={heroContainerRef}
                     className="bg-primary transition  hover:bg-transparent text-secondary hover:text-primary border border-green-800 rounded-full md:my-5 md:mx-2 ml-10 my-6 px-8 py-2 justify-start animate-moveUp-btn">
-                    <a href="/">Shop now</a>
+                    <a href="https://aureliara.store/skincare/anti-imperfections">
+                      Shop now
+                    </a>
                   </button>
                 </div>
               </div>
@@ -105,8 +107,6 @@ export default function Preload() {
           </div>
         )}
       </div>
-      
-        
     </section>
   );
 }
